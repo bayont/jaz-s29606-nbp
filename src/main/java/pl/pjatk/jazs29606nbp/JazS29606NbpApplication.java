55d9402141b8bc891c5c0d7c89e44187efe92873
fabian.fetter <fabian.fetter@woodpecker.co>
2024-06-29 12:33:43 +0200

    initial commit

---
+++ src/main/java/pl/pjatk/jazs29606nbp/JazS29606NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs29606nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS29606NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS29606NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/jazs29606nbp/JazS29606NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs29606nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS29606NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
